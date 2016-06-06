fe941c50e0fc914039da4c39d4beb363a105d209
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-06-05 23:41:16 -0500

    Add inquirer

--- slushfile.js
+++ slushfile.js
@@ -1,12 +1,31 @@
 var gulp = require("gulp");
 var install = require("gulp-install");
 var conflict = require("gulp-conflict");
+var inquirer = require("inquirer");
 
 gulp.task("default", (done) => {
-  gulp.src(__dirname + "/hello-component/**")
-    .pipe(conflict("./hello-component"))
-    .pipe(gulp.dest("./hello-component"))
-    .pipe(install())
-    .on("end", done)
-    .resume();
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of your component?",
+      default: "hello-component"
+    },
+    {
+      type: "confirm",
+      name: "confirm",
+      message: "Create component?"
+    }
+  ], function (answers) {
+    if (!ansers.confirm) {
+      return done();
+    }
+
+    gulp.src(__dirname + "/hello-component/**")
+      .pipe(conflict("./" + answers.name))
+      .pipe(gulp.dest("./" + answers.name))
+      .pipe(install())
+      .on("end", done)
+      .resume();
+  });
 });
