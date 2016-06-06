5dc228f1345274e31ce4150f157345ff53851bc3
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-06-05 23:33:12 -0500

    Confirm overwrites on conflict

--- slushfile.js
+++ slushfile.js
@@ -1,8 +1,10 @@
 var gulp = require("gulp");
 var install = require("gulp-install");
+var conflict = require("gulp-conflict");
 
 gulp.task("default", (done) => {
   gulp.src(__dirname + "/hello-component/**")
+    .pipe(conflict("./hello-component"))
     .pipe(gulp.dest("./hello-component"))
     .pipe(install())
     .on("end", done)
