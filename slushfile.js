a6557ce9fe839ab5f76961a141073a1dd42e4147
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-06-05 23:23:13 -0500

    Fix directory

--- slushfile.js
+++ slushfile.js
@@ -1,7 +1,7 @@
 let gulp = require("gulp");
 
 gulp.task("default", (done) => {
-  gulp.src(__dirname + "hello-component/**")
+  gulp.src(__dirname + "/hello-component/**")
     .pipe(gulp.dest("./"))
     .on("end", done)
     .resume();
