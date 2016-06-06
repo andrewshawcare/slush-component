2b37fd311d73ac6508d5d552eb118c939895265a
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-06-05 23:27:30 -0500

    Run install after copy; Copy into own folder

--- slushfile.js
+++ slushfile.js
@@ -1,8 +1,10 @@
-let gulp = require("gulp");
+var gulp = require("gulp");
+var install = require("gulp-install");
 
 gulp.task("default", (done) => {
   gulp.src(__dirname + "/hello-component/**")
-    .pipe(gulp.dest("./"))
+    .pipe(gulp.dest("./hello-component"))
+    .pipe(install())
     .on("end", done)
     .resume();
 });
